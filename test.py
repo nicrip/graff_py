9c0414c11867237276325b4d972446f16c86e29f
Pedro Vaz Teixeira <pvazteixeira@gmail.com>
2019-04-06 22:17:36 -0400

    validated multiplyDistributions.

--- test.py
+++ test.py
@@ -7,16 +7,29 @@ from graff.Distribution.BallTreeDensity import BallTreeDensity
 
 from graff.Core import MultiplyDistributions
 
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
     e = Endpoint()
 
     e.Connect('tcp://192.168.0.102:5555')
     print(e.Status())
 
-    b1 = BallTreeDensity('Gaussian', np.ones(10), np.ones(10), np.random.randn(10))
-    b2 = BallTreeDensity('Gaussian', np.ones(10), np.ones(10), np.random.randn(10))
+    N = 1000
+    u1 = 0.0
+    s1 = 3.0
+
+    u2 = 50.0
+    s2 = 3.0
+    b1 = BallTreeDensity('Gaussian', np.ones(N), np.ones(N), u1+s1*np.random.randn(N))
+    b2 = BallTreeDensity('Gaussian', np.ones(N), np.ones(N), u2+s2*np.random.randn(N))
 
     r = MultiplyDistributions(e, [b1,b2])
-    print(r)
+    print(r['payload'])
+
+    x = np.array(r['payload']['points'] )
+    # plt.stem(x, np.ones(len(x)) )
+    plt.hist(x, bins = int(len(x)/10.0))
+    plt.show()
 
     e.Disconnect()
